3c431901235096dc5bc68adc3642ee689f21519f
ReshapeDream <122186126@qq.com>
2018-01-16 17:08:34 +0800

    更新gradle

--- app/src/main/java/com/neil/demo/parallaxsplashdemo/MainActivity.java
+++ app/src/main/java/com/neil/demo/parallaxsplashdemo/MainActivity.java
@@ -4,10 +4,10 @@ import android.graphics.drawable.AnimationDrawable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
-import com.neil.parallaxsplash.ParallaxContainer;
+import com.neil.library.parallaxsplash.ParallaxContainer;
+
 
 public class MainActivity extends AppCompatActivity {
     private ParallaxContainer container;
